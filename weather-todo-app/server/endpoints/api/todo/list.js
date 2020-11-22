7876aff5712ef79d050ec0facbb3ed32a70e71bb
Arkadiusz Kałkus <kalkus@10g.pl>
2020-11-22 21:04:44 +0100

    Listing todos from db in API

--- weather-todo-app/server/endpoints/api/todo/list.js
+++ weather-todo-app/server/endpoints/api/todo/list.js
@@ -1,5 +1,4 @@
 
-import AppException from '../../../exceptions/AppException.js';
 
 /**
  * Handles list todo request
@@ -8,8 +7,14 @@ import AppException from '../../../exceptions/AppException.js';
  * @property {Response} res response object
  * @returns {Promise<Object|String>}
  */
-export default function todoListHandler() {
-    return AppException.reject('Internal error', 'Listing todos not implemented');
+// export default function todoListHandler() {
+//     return AppException.reject('Internal error', 'Listing todos not implemented');
+// }
+
+export default function todoListHandler(server, req, res) {
+    return server.db.models()
+        .then(models => models.Todo.findAll())
+        .then(result => ({ data: result }));
 }
 
 
